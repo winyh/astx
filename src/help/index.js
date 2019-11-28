5d764592b590306f918b93d67435e4aa2c3c789f
winyh <1124692067@qq.com>
2019-11-28 11:37:55 +0800

    预置组件拖拽时，生成组件描述JSON数据

--- src/help/index.js
+++ src/help/index.js
@@ -1,12 +1,16 @@
 import * as antd from 'antd';
+const uuidv4 = require('uuid/v4');
+
+const generateUUID = () => uuidv4()
 
 const getDefaultProps = (name) => antd[name].defaultProps
 
 const generateInitJson = (tag) => (
-	{
+	{	
+		id:generateUUID(),
 		type:tag,
 		props:{
-			...getDefaultProps(tag)
+			...getDefaultProps(tag),
 		},
 		children:[
 
@@ -15,6 +19,7 @@ const generateInitJson = (tag) => (
 )
 
 export {
+	generateUUID,
 	getDefaultProps,
 	generateInitJson
 }
