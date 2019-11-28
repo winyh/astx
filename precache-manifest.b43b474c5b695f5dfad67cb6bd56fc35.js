ed7c124eb210a7f2e205897291723dda4bdb985e
winyh <1124692067@qq.com>
2019-11-28 14:09:11 +0800

    Updates

---
+++ precache-manifest.b43b474c5b695f5dfad67cb6bd56fc35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89adc650abe3cc9d367cf15ea2dbf59c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ef061e9588586c93a19",
+    "url": "/static/css/2.90172c5a.chunk.css"
+  },
+  {
+    "revision": "d68528fb70897cdfb6b8",
+    "url": "/static/css/main.97a5597e.chunk.css"
+  },
+  {
+    "revision": "2ef061e9588586c93a19",
+    "url": "/static/js/2.190ba09e.chunk.js"
+  },
+  {
+    "revision": "d68528fb70897cdfb6b8",
+    "url": "/static/js/main.f01eef1d.chunk.js"
+  },
+  {
+    "revision": "51d2be9524d992a37e32",
+    "url": "/static/js/runtime-main.c51f125f.js"
+  }
+]);
